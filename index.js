6dc2f6e5a79f28369015e8e3cccbe53b32e486ce
FaustLuck <faustluck91@gmail.com>
2021-02-08 22:58:44 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+showFile = async (input) => {
+    await new Response(input.files[0]).json().then(json => {
+        for (let i = 0; i < json.length; i++) {
+            let div = document.createElement('div');
+            let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+            svg.setAttributeNS("http://www.w3.org/2000/xmlns/", "xmlns:xlink", "http://www.w3.org/1999/xlink");
+            svg.id = `barcode${i}`;
+            div.append(svg)
+            document.body.append(div);
+            JsBarcode(`#${svg.id}`, json[i], { fontSize: 25 });
+        }
+    }, e => {
+        console.log('Не JSON файл!!!')
+    })
+    document.body.removeChild(input)    
+}
+
+
