9669dfb89ac096f6d2baf84b6ec9c819fb96c447
FaustLuck <faustluck91@gmail.com>
2021-02-09 21:03:36 +0300

    master

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-showFile = async (input) => {
-    await new Response(input.files[0]).json().then(json => {
-        for (let i = 0; i < json.length; i++) {
-            let div = document.createElement('div');
-            let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-            svg.setAttributeNS("http://www.w3.org/2000/xmlns/", "xmlns:xlink", "http://www.w3.org/1999/xlink");
-            svg.id = `barcode${i}`;
-            div.append(svg)
-            document.body.append(div);
-            JsBarcode(`#${svg.id}`, json[i], {
-                fontSize: 25,
-                margin: 0,
-                height: 75
-            });
+generate = (string) => {
+    let arrayOfAdress = string.split(' '),
+        options = {
+            fontSize: 25,
+            margin: 0,
+            height: 75
         }
-    }, e => {
-        console.log('Не JSON файл!!!')
-    })
-    document.body.removeChild(input)
+    for (let i = 0; i < arrayOfAdress.length; i++) {
+        JsBarcode(`#${createBarcodeContainer(i)}`, arrayOfAdress[i], options)
+    }
+    document.body.removeChild(document.forms.inputForm)
 }
 
-
+createBarcodeContainer = (i) => {
+    let div = document.createElement('div'),
+        svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.setAttributeNS("http://www.w3.org/2000/xmlns/", "xmlns:xlink", "http://www.w3.org/1999/xlink");
+    svg.id = `barcode${i}`;
+    div.append(svg)
+    document.body.append(div);
+    return svg.id
+}
